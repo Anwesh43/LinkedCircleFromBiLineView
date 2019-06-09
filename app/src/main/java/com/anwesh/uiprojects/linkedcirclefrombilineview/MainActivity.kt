2c499576e8eb9ec536cd6557855685fb64c109a3
Anwesh43 <anweshthecool0@gmail.com>
2019-06-10 03:34:59 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedcirclefrombilineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcirclefrombilineview/MainActivity.kt
@@ -3,12 +3,13 @@ package com.anwesh.uiprojects.linkedcirclefrombilineview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import com.anwesh.uiprojects.circlefrombilineview.CircleFromBiLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CircleFromBiLineView.create(this)
         fullScreen()
     }
 }
