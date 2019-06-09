310de69019b20d726938fc421201023c8de87a02
Anwesh43 <anweshthecool0@gmail.com>
2019-06-10 03:03:42 +0530

    adding libraries and constants

--- circlefrombilineview/src/main/java/com/anwesh/uiprojects/circlefrombilineview/CircleFromBiLineView.kt
+++ circlefrombilineview/src/main/java/com/anwesh/uiprojects/circlefrombilineview/CircleFromBiLineView.kt
@@ -3,3 +3,23 @@ package com.anwesh.uiprojects.circlefrombilineview
 /**
  * Created by anweshmishra on 10/06/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.9f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#283593")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val sweepDeg : Float = 360f
+val rFactor : Float = 3f
